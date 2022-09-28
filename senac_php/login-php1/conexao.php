035d6468d1850e606b2429cee99ab326b0eee71a
John Erick <63692868+johnerick-py@users.noreply.github.com>
2022-09-28 16:11:40 -0300

    deletando, atualizei pelo navegador pasta login

--- senac_php/login-php1/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('HOST', '127.0.0.1');
-define('USUAIO', 'root');
-define('SENHA','');
-define('DB','login');
-
-$conexao = mysqli_connect(HOST, USUAIO, SENHA, DB) or die("Não foi possível se conectar ao banco de dados!");
