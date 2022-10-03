275950d4d67471d42982a25e1b40f26874c1310b
John Erick <63692868+johnerick-py@users.noreply.github.com>
2022-10-03 15:00:02 -0300

    deletando
    deletando para teste site

--- login-php1/check_login.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if(!$_SESSION['usuario']){
-  header('Location: index.php');
-  exit();
-}
-
-?>
--- login-php1/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy(); #???
-$_SESSION['usuario'] = "";
-header('Location: index.php');
-exit();
-?>
